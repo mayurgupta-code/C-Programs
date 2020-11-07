6eb662f3fbb072e56c7f02d5788cdf2f6fc51241
Mayur Gupta <56781975+coddiction@users.noreply.github.com>
2020-11-07 19:04:10 +0530

    Adaptive is also added in bubble sort

---
+++ Algorithms/bubble_sort_adaptive.c
@@ -0,0 +1,63 @@
+//BUBBLE Sort
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+void PrintArray(int *arr, int size){
+  for (int i=0; i<size; i++){
+    printf("%d ",arr[i]);
+  }
+  printf("\n");
+}
+
+//here we compare the two elements of the array at a time
+//like 0,1 1,2 2,3 3,4 this type by int c=0 and c+1
+//first for loop is for "passes" that is (n-1) to how much time for bubble sort need to be passes
+//second while loop is for to execute a particular pass
+//in second loop firstly in first pass n-1 comparisons and swaps are there
+//then comp. and swaps goes on decreasing successively after each passes
+
+void bubbleSort(int *arr, int size){
+  int temp = 0; //temp is for to swap the two elements of the array
+  int k = 0;
+  for (int i=0; i<(size-1); i++){
+    int c = 0;
+    int j = (size-1);
+    while( j != 0 ){ // j<n-i-1
+      if (arr[c] > arr[c+1]){
+        k++;
+        temp = arr[c+1];
+        arr[c+1] = arr[c];
+        arr[c] = temp;
+      }
+      c++;
+      j = j-1;
+    }
+    //due to this array is adaptive
+    //that means this bubble sort exit in one pass if array is already sorted
+    if (k == 0){
+      printf("Array is already sorted !!\n");
+      break;
+    }
+  }
+  //no need of this becoz. change is made in the original array
+  //PrintArray(arr, size);
+}
+
+
+int main(){
+  //int arr[] = {7,11,9,2,17,4};
+  int arr[] = {1,2,3,4,5,6,7};
+  int size = sizeof(arr)/sizeof(int);
+
+  printf("Array before Bubble Sort : ");
+  PrintArray(arr, size);
+
+  bubbleSort(arr, size);
+
+  printf("Array after Bubble Sort : ");
+  PrintArray(arr, size);
+
+
+  return 0;
+}
